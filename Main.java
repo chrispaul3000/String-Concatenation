dab46122d5d80a0abc851e2f6d687f17c3ac6418
chrispaul3000 <cristian.tolibas10@gmail.com>
2024-11-25 19:36:00 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg2narraytolibas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author user
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+
+      
+        System.out.print("Enter the number of rows: ");
+        int rows = scanner.nextInt();
+        System.out.print("Enter the number of columns: ");
+        int cols = scanner.nextInt();
+
+      
+        int[][] array = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                array[i][j] = i * j;
+            }
+        }
+        
+        System.out.println("Generated Multiplication Table:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(array[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    
+    }
+    
+}
