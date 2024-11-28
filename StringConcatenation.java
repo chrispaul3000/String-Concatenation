dbd2861f705f2a6ff531ef001f4f9a4ef801ce23
chrispaul3000 <cristian.tolibas10@gmail.com>
2024-11-28 15:39:07 +0800

    Update and rename Main.java to StringConcatenation.java

---
+++ StringConcatenation.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package string.concatenation;
+
+/**
+ *
+ * @author user
+ */
+public class StringConcatenation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String First = "Christian";
+        String Last = "Tolibas";
+        System.out.print("My name is" + First +" "+ Last);
+    }
+    
+}
